1fdd3a63076ae186854ce1b3628c3e810684518c
Dania <daniatub8@gmail.com>
2022-04-20 13:45:50 +0300

    Given an integer array nums of length n, // you want to create an array ans of length 2n where ans[i] == nums[i] and // ans[i + n] == nums[i] for 0 <= i < n

--- src/com/Dania/Problems/One.java
+++ src/com/Dania/Problems/One.java
@@ -1,6 +1,33 @@
 package com.Dania.Problems;
 
-public class One {
+import com.sun.tools.javac.util.ArrayUtils;
 
+import java.util.Arrays;
+
+//Given an integer array nums of length n,
+// you want to create an array ans of length 2n where ans[i] == nums[i] and
+// ans[i + n] == nums[i] for 0 <= i < n
+public class One {
+    public int[] getConcatenation(int[] nums)
+    {
+        int size =nums.length;
+
+        int[] ans = new int[2*size];
+        int j=0;
+
+        for(int i=0;i<size;i++)
+        {
+            ans[j] = nums[i];
+            j++;
+        }
+
+        for(int i=0;i<size;i++)
+        {
+            ans[j]=nums[i];
+            j++;
+        }
+
+        return ans;
+    }
 
 }
